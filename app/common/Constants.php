d094d2eb47ef8cb0c3bf2754d086684d8bd8b235
andresjrkalev <andresjrkalev@gmail.com>
2020-01-03 13:20:51 +0200

    Create Constants.php

---
+++ app/common/Constants.php
@@ -0,0 +1,43 @@
+<?php
+
+const CONFIGURATION_DISPLAY_ERRORS = "display_errors";
+const CONFIGURATION_ERROR_LOG = "error_log";
+const CONSTANT_PROPERTIES = "PROPERTIES";
+const CRITERIA_EQUALS = "{0} = ?";
+
+const DATE_Y_M_D_H_I_S = "Y-m-d H:i:s";
+const DATA_SOURCE = "{0}:host={1};dbname={2}";
+const DATABASE_DRIVER = "database.driver";
+const DATABASE_HOST = "database.host";
+const DATABASE_NAME = "database.name";
+const DATABASE_PASSWORD = "database.password";
+const DATABASE_TABLE = "{0}s";
+const DATABASE_USERNAME = "database.username";
+
+const ENVIRONMENT_PRODUCTION = "production";
+
+const FORMATTER_LANG_ET = "et_EE";
+const FUNCTION_HANDLE_ERROR = "handleError";
+
+const LOCATION_LOGS = "app/logs.txt";
+
+const METHOD_GETTER = "get{0}";
+const METHOD_INDEX = "index";
+const METHOD_SETTER = "set{0}";
+
+const PATH_ENTITY = "../model/{0}.php";
+const PATH_ROOT = "/";
+const PATTERN_CONTROLLER = "{0}{1}";
+const PATTERN_FILE_INI = "{0}.ini";
+const PATTERN_FILE_PHP = "{0}.php";
+const PATTERN_LOG = "<b>Error:</b> [nr {0}] {1} in file {2} on line {3}";
+
+const QUERY_SELECT_CRITERIA = "SELECT * FROM {0} WHERE {1} AND deleted IS NULL";
+const QUERY_UPDATE = "UPDATE {0} SET {1} WHERE id = ?";
+
+const REGEX_LOCALHOST = "/localhost|127.0.0.1/";
+
+const STRING_EMPTY = "";
+const SUFFIX_CONTROLLER = "Controller";
+const SYMBOL_COMMA = ",";
+
